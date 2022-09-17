c2e9c5b18849764ae66a7c90caccb1b06378ee40
Chandrashekar Dasari <chandra.dasari.cs@gmail.com>
2022-09-17 09:02:11 -0500

    add video feed

--- src/components/camera.js
+++ src/components/camera.js
@@ -1,11 +1,35 @@
+import React, { useEffect, useState, useRef } from 'react';
 import Webcam from "react-webcam";
+import { detectFaces, drawResults} from './utils/faceApi';
+import Results from './results';
 
 const Camera = () => {
+    const camera = useRef();
+    const cameraCanvas = useRef();
+    const [results,setResults] = useState([]);
+    const getFaces = async () => {
+        // check if the frame exists
+        if (camera.current !== null) {
+            // looks for the faces in the frame
+            const seenFaces = await detectFaces(camera.current.video);
+            await drawResults(camera.current.video, cameraCanvas.current,seenFaces,'box');
+            setResults(seenFaces)
+            console.log(seenFaces)
+        }
+    }
+    useEffect(() => {
+        // fetches faces in the frame every 50ms
+        const interval = setInterval(async() => {
+            await getFaces();
+        }, 50);
+        return () => clearInterval(interval);
+    }, [results])
 
     return (
         <div>
-            <Webcam width="800px" height="auto"/>
+            <Webcam ref={camera} width="800px" height="auto"/>
         </div>
     )
 }
+
 export default Camera
---
+++ src/components/results.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Results = ({results,processing}) => {
+    if (processing && results) {
+        <h1>loading...</h1>
+    }
+    if (!processing && results) {
+        return (
+            <div>
+                <h1>Results</h1>
+                <p>Number of faces: {results.length}</p>
+                <p>Expression: {results[0].expressions.asSortedArray()[0].expression}</p>
+            </div>
+        );
+    }
+    else{
+        return (
+            <div>
+                <h1>Results</h1>
+                <p>No faces detected</p>
+            </div>
+        )
+    }
+}
+
+export default Results;
