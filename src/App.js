ebb5ca03c4b45d4e5a041861c2de375ac62f4752
Chandrashekar Dasari <chandra.dasari.cs@gmail.com>
2022-09-17 08:28:46 -0500

    got camera working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import Camera from './components/camera'
 
 function App() {
-  let variable = "sup"
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='App-heading'>
           How you feelin? 
         </h1>
-        
       </header>
+      <Camera></Camera>
+      <p>
+        Here goes bottom text
+      </p>
     </div>
   );
 }
---
+++ src/components/camera.js
@@ -0,0 +1,11 @@
+import Webcam from "react-webcam";
+
+const Camera = () => {
+
+    return (
+        <div>
+            <Webcam width="800px" height="auto"/>
+        </div>
+    )
+}
+export default Camera
