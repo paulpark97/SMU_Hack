e6470d4f057837cd8f2a203316fe5284672e34cc
Chandrashekar Dasari <chandra.dasari.cs@gmail.com>
2022-09-18 05:43:24 -0500

    merge saja's changes

---
+++ src/about.js
@@ -0,0 +1,15 @@
+export default function About(){
+    
+    return (
+        <div>
+            <div className="About-dv-hd">
+                <h1 className="About-header">
+                    The Vibe Check Story
+                </h1>
+            </div>
+            <p>
+                cus i miss us.
+            </p>
+        </div>
+    )
+}
